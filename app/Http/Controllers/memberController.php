89ec5388a43ee6c1237d475e1f46e0e88592ce8b
Vu Nguyen <ndhungvu@gmail.com>
2023-05-12 16:12:24 +0000

    memberController.php edited online with Bitbucket

--- app/Http/Controllers/memberController.php
+++ app/Http/Controllers/memberController.php
@@ -10,47 +10,47 @@ class memberController extends Controller
 {
     private $recruitmentRepository;
 
-    public function __construct(RecruitmentRepositoryInterface $recruitmentRepository ,Request $request)
+    public function __construct(RecruitmentRepositoryInterface $recruitmentRepository ,Request $request) //sai request làm gì
     {
         $this->recruitmentRepository = $recruitmentRepository;
     }
 
-    public function index(){
+    public function index(){ //sai convention
         if (Auth::check()) {
             $data =  $this->recruitmentRepository->allRecruitments();
-            return view('admin.home', ['result' =>$data])->with('i',(request()->input('page',1)-1)*5);
+            return view('admin.home', ['result' =>$data])->with('i',(request()->input('page',1)-1)*5);//cái gì đây mục đích và sai convention
              
         } else {
-            return view('welcome')->with('success','Vui Lòng Đăng nhập!');
+            return view('welcome')->with('success','Vui Lòng Đăng nhập!'); // cách ra với lại ko check kiểu này
         }
-    }
-    public function log_out(){
+    }// cách xuốnng 
+    public function log_out(){ //sai convention
         Auth::logout();
         return view('welcome');
-    }
+    }//sai convention
     public function login(Request $request){
         if($request->has('email') && $request->has('password')){
             $data =[
                 'email' => $request->email,
                 'password' => $request->password,
             ];
-             if(Auth::attempt($data)){
+             if(Auth::attempt($data)){//cái lùm chi chổ ni ?
              return redirect()->route('index')->with('i',(request()->input('page',1)-1)*5);
              }else{
                 return redirect()->back()->with('success','Sai tài khoản hoặc mật khẩu vui lòng đăng nhập lại !');
              }
-            }
-    }
+            }//sai convention
+    }// cách xuống 
     public function recover_pass(Request $request){
         $data = $request->all();
         $this->recruitmentRepository->recoverPass($data);
         return redirect()->back()->with('message','Gửi mail thành công vui lòng vào email để reset pass');
-        
-    }
+        //gì đây ?
+    }//cách xuống 
     public function update_new_pass(Request $request){
         $data = $request->all();
-        $this->recruitmentRepository->updatePass($data);
+        $this->recruitmentRepository->updatePass($data);//cách xuống1 line rồi return
         return view('welcome');
     }
-   
+   // gì đây 
 }
