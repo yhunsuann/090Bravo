c0b5079d12e1e30972f29fe7f76ef3518bfa6e36
Vu Nguyen <ndhungvu@gmail.com>
2023-05-12 16:08:36 +0000

    RecruitmentRepository.php edited online with Bitbucket

--- app/Repositories/RecruitmentRepository.php
+++ app/Repositories/RecruitmentRepository.php
@@ -12,16 +12,16 @@ use Illuminate\Support\Facades\Hash;
 
 class RecruitmentRepository implements RecruitmentRepositoryInterface
 {
-
+//xoá line trống với ở repository ko sử dụng trực tiếp model
     public function allRecruitments()
     {
-        return Recruitments::where('deleted_at',null)->paginate(5);
+        return Recruitments::where('deleted_at',null)->paginate(5); // sai tìm hiểu ORM
     }
 
     public function addRecruitments($data)
     {
         return Recruitments::create($data);
-    }
+    }// cách xuông 
     public function updateCruitments($data, $id)
     {
         Recruitments::Where('id', $id)->update($data);
@@ -30,13 +30,13 @@ class RecruitmentRepository implements RecruitmentRepositoryInterface
     public function deleteCruitments($id)
     {
         $data = array();
-        $data['deleted_at'] = now();
+        $data['deleted_at'] = now();//sai ko update kiểu này tìm hiểu softdelete
         Recruitments::Where('id', $id)->update($data);
     }
    
     public function editCruitments($id){
         return Recruitments::where('id',$id)->get();
-    }
+    } // chưa cách dòng 
     public function searchCruitments($data)
     {  
         $status = $data['status'];
@@ -45,28 +45,28 @@ class RecruitmentRepository implements RecruitmentRepositoryInterface
         $dateTo = $data['dateTo'];
        return Recruitments::when($keyword, function ($q) use ($keyword) {
             return $q->where('title', 'like', '%'.$keyword.'%');
-        })->when($status, function($q) use ($status) {
+        })->when($status, function($q) use ($status) { // sai thụt indent vô
             return $q->where('status', $status);
         })->when($dateFrom, function($q) use ($dateFrom,$dateTo) {
             return $q->whereBetween('created_at', [$dateFrom, $dateTo]);
         })->paginate();
-    }
+    }// cách line 
     public function deleteSelect($ids){
         $data = array();
         $data['deleted_at'] = now();
         Recruitments::WhereIn('id',$ids)->update($data);;    
-    }
+    } // cách line 
     public function recoverPass($data){
         $email = $data['email'];
         $now = Carbon::now('Asia/Ho_Chi_Minh')->format('d-m-Y');
         $title_mail = "Lấy Lại Mật Khâu". ' '.$now;
         $customer = User::where('email','=',$data['email'])->get();
-        $customer_id = $customer[0]->getoriginal('id');
+        $customer_id = $customer[0]->getoriginal('id'); //cách line 
         if($customer){
             $count_customer = $customer->count();
             if( $count_customer == 0){
                 return redirect()->back();
-            }else{
+            }else{ // cách ra 
                 $token_random = bin2hex(Str::random(20));
                 $data = array();
                 $data['token'] = $token_random;
@@ -78,7 +78,7 @@ class RecruitmentRepository implements RecruitmentRepositoryInterface
                     "name"=>$title_mail,
                     "body"=>$link_reset_pass,
                     'email'=>$email
-                );
+                );//cáhc xuóng
                 Mail::send('admin.forget_pass_notify',['data'=>$data],function($message) use($title_mail,$data){
                     $message -> to($data['email'])->subject($title_mail);
                     $message ->from('bphuoc.20it10@vku.udn.vn',$title_mail);
@@ -86,7 +86,7 @@ class RecruitmentRepository implements RecruitmentRepositoryInterface
                
             }     
         }
-    }
+    }//cách xuống 
     public function updatePass($data){
         $new_pass = $data['new_pass'];
         $token = $data['token'];
@@ -94,5 +94,5 @@ class RecruitmentRepository implements RecruitmentRepositoryInterface
         $data['password'] = HASH::make($new_pass);
         User::Where('token',$token)->update($data);
     }
-    
+    //xoá line 
 }
