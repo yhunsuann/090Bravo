43c53cc9883859b0540aa073fa94a981c618ea9d
yhunsuann <bphuoc.20it10@vku.udn.vn>
2023-05-18 19:08:59 +0700

    DB

--- DB_Recruitments.sql
+++ DB_Recruitments.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 54.219.4.93    Database: project1
 -- ------------------------------------------------------
--- Server version	8.0.33-0ubuntu0.22.04.1
+-- Server version	8.0.33-0ubuntu0.22.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `languages`
+--
+
+DROP TABLE IF EXISTS `languages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `languages` (
+  `language_code` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `language_name` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`language_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `languages`
+--
+
+LOCK TABLES `languages` WRITE;
+/*!40000 ALTER TABLE `languages` DISABLE KEYS */;
+INSERT INTO `languages` VALUES ('en','English'),('vi','VietNam');
+/*!40000 ALTER TABLE `languages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -27,7 +51,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +60,7 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'2019_12_14_000001_create_personal_access_tokens_table',1),(2,'2023_05_10_125504_users',1),(3,'2023_05_10_130704_recruitments',1),(4,'2023_05_10_133015_recruitment_translates',1),(5,'2023_05_10_140432_recruitments',2),(6,'2023_05_10_140505_recruitment_translates',2),(7,'2023_05_10_150935_recruitments',3),(8,'2023_05_10_151109_recruitment_translates',4);
+INSERT INTO `migrations` VALUES (1,'2019_12_14_000001_create_personal_access_tokens_table',1),(2,'2023_05_10_125504_users',1),(3,'2023_05_10_150935_recruitments',1),(4,'2023_05_17_131104_languages',1),(5,'2023_05_17_131642_recruitment_translate',1);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,15 +106,17 @@ DROP TABLE IF EXISTS `recruitment_translates`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `recruitment_translates` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `language_code` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
   `recruitment_id` int unsigned NOT NULL,
+  `language_code` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `recruitment_translates_recruitment_id_foreign` (`recruitment_id`),
+  KEY `recruitment_translates_language_code_foreign` (`language_code`),
+  CONSTRAINT `recruitment_translates_language_code_foreign` FOREIGN KEY (`language_code`) REFERENCES `languages` (`language_code`) ON DELETE CASCADE,
   CONSTRAINT `recruitment_translates_recruitment_id_foreign` FOREIGN KEY (`recruitment_id`) REFERENCES `recruitments` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,6 +125,7 @@ CREATE TABLE `recruitment_translates` (
 
 LOCK TABLES `recruitment_translates` WRITE;
 /*!40000 ALTER TABLE `recruitment_translates` DISABLE KEYS */;
+INSERT INTO `recruitment_translates` VALUES (1,28,'en','Developer PHP (Laravel, CakePHP, EC Cube)','<p>Developer&nbsp; PHP (Laravel, CakePHP, EC Cube)</p>','Developer  PHP (Laravel, CakePHP, EC Cube)'),(2,28,'vi','Nhà phát triển PHP (Laravel, CakePHP, EC Cube)','<p>Nhà phát triển PHP (Laravel, CakePHP, EC Cube)</p>','Nhà phát triển PHP (Laravel, CakePHP, EC Cube)'),(3,29,'en','Dev Web Frontend','<p>Dev Web Frontend</p>','Dev Web Frontend'),(4,29,'vi','Lập Trình Viên Web Frontend Fresher','<p>Lập Trình Viên Web Frontend</p>','Lập Trình Viên Web Frontend'),(5,30,'en','Fresher Data Engineer','<p>Fresher Data Engineer&nbsp;</p>','Fresher Data Engineer'),(6,30,'vi','Chuyên Viên Data Engineer','<p>Chuyên Viên Data Engineer&nbsp;</p>','Chuyên Viên Data Engineer'),(7,31,'en','Data Engineer','<p>Data Engineer&nbsp;</p>','Data Engineer'),(8,31,'vi','Kỹ thuật viên Data Engineer','<p>Kỹ thuật viên Data Engineer&nbsp;</p>','Kỹ thuật viên Data Engineer'),(9,32,'en','Developer PHP (Laravel, CakePHP, EC Cube)','<p>Developer PHP (Laravel, CakePHP, EC Cube)</p>','Developer PHP (Laravel, CakePHP, EC Cube)'),(10,32,'vi','Nhà phát triển PHP (Laravel, CakePHP, EC Cube)','<p>Nhà phát triển PHP (Laravel, CakePHP, EC Cube)</p>','Nhà phát triển PHP (Laravel, CakePHP, EC Cube)'),(11,33,'en','qưeqw','<p>qưe</p>','qưe'),(12,33,'vi','qưe','<p>qưe</p>','qưe'),(13,34,'en','qưe','<p>qưe</p>','qưe'),(14,34,'vi','qưe','<p>qưe</p>','qưe'),(15,35,'en','Dev FE ReactJs','<p>Dev FE ReactJs</p>','Dev FE ReactJs'),(16,35,'vi','Lập trình viên FE ReactJs','<p>Lập trình viên FE ReactJs</p>','Lập trình viên FE ReactJs'),(17,37,'en','BUI PHUOC en','<p>BUI PHUOC en</p>','BUI PHUOC en'),(18,37,'en','BUI PHUOC en','<p>BUI PHUOC en</p>','BUI PHUOC en'),(19,37,'vi','BUI PHUOC vi','<p>BUI PHUOC vi</p>','BUI PHUOC vi'),(20,38,'en','Title en','<p>Title en</p>','Title en'),(21,38,'en','Title en','<p>Title en</p>','Title en'),(22,38,'vi','Title vi','<p>Title vi</p>','Title vi'),(23,39,'en','BUI PHUOC vi','<p><span style=\"color: rgba(44, 56, 74, 0.95); font-size: medium;\">BUI PHUOC vi</span></p>','BUI PHUOC vi'),(24,39,'vi','BUI PHUOC vi','<p><span style=\"color: rgba(44, 56, 74, 0.95); font-size: medium;\">BUI PHUOC vi</span></p>','BUI PHUOC vi'),(25,41,'en','Title en','<p>Title en</p>','Title en'),(26,41,'vi','Title en','<p>Title en</p>','Title en'),(27,42,'en','Developt Mobile','<p>Developt Mobile</p>','Developt Mobile'),(28,42,'vi','Lập Trình Viên Mobile','<p>Lập Trình Viên Mobile</p>','Lập Trình Viên Mobile');
 /*!40000 ALTER TABLE `recruitment_translates` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -111,15 +138,12 @@ DROP TABLE IF EXISTS `recruitments`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `recruitments` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `image` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` enum('Active','UnActive','Expired','Closed') COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NOT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +152,7 @@ CREATE TABLE `recruitments` (
 
 LOCK TABLES `recruitments` WRITE;
 /*!40000 ALTER TABLE `recruitments` DISABLE KEYS */;
-INSERT INTO `recruitments` VALUES (1,'zxc','zxc','zxc','1683731601-img.png','Expired','2023-05-10 00:00:00','2023-05-11 02:06:18'),(2,'ReactJs','ReactJs','ReactJs','1683731629-img.jpg','Active','2023-05-06 00:00:00',NULL),(3,'[Urgent] Ruby On Rails Engineer (Senior/Principal)','[Urgent] Ruby On Rails Engineer (Senior/Principal)','[Urgent] Ruby On Rails Engineer (Senior/Principal)','1683769230-img.webp','Active','2023-05-11 00:00:00','2023-05-11 02:59:17'),(4,'Professional Mobile Engineer (Javascript, HTML5)','Professional Mobile Engineer (Javascript, HTML5)','Professional Mobile Engineer (Javascript, HTML5)','1683769272-img.jpg','Expired','2023-05-11 00:00:00','2023-05-11 02:59:29'),(5,'Senior/Associate Business Product Management','Senior/Associate Business Product Management','Senior/Associate Business Product Management','1683769325-img.png','Active','2023-05-11 00:00:00','2023-05-11 06:49:30'),(6,'Backend Java Engineer (Senior and above)','Backend Java Engineer (Senior and above)','Backend Java Engineer (Senior and above)','1683769398-img.jpg','Expired','2023-05-11 00:00:00','2023-05-11 03:36:39'),(7,'zxc','zxc','zxc','1683779287-img.png','UnActive','2023-05-11 00:00:00','2023-05-11 06:48:41'),(8,'zxc','zxc','zxc','1683779371-img.png','Active','2023-05-11 00:00:00','2023-05-11 06:48:41'),(9,'zxc','zxc','zxc','1683779440-img.png','Active','2023-05-11 00:00:00','2023-05-11 06:48:41'),(10,'czx','zxc','zxc','1683779696-img.jpg','Active','2023-05-11 00:00:00','2023-05-11 06:48:41'),(11,'zxc','zxc','zxc','1683779774-img.png','Active','2023-05-11 00:00:00','2023-05-11 06:49:30'),(12,'zxc','zxc','zxc','1683779911-img.png','Active','2023-05-11 00:00:00','2023-05-11 06:49:30'),(13,'zxc','zxc','zxc','1683780044-img.jpg','Active','2023-05-11 00:00:00','2023-05-11 06:49:30'),(14,'vava','avav','avav','1683780278-img.jpg','Active','2023-05-11 00:00:00','2023-05-11 07:16:06'),(15,'qrqr','qrqr','qrqr','1683780423-img.png','Active','2023-05-11 00:00:00','2023-05-11 06:52:44'),(16,'qtqt','qt','qt','1683787865-img.png','Active','2023-05-11 00:00:00','2023-05-11 07:16:06'),(17,'rqrq','qrqr','qrqr','1683787984-img.webp','Expired','2023-05-11 00:00:00','2023-05-11 07:16:06'),(18,'fqw','eq','fqfw','1683788071-img.jpg','Active','2023-05-11 00:00:00','2023-05-11 06:55:34'),(19,'gaga','gaag','gaga','1683788120-img.png','Active','2023-05-11 00:00:00','2023-05-11 06:59:13'),(20,'zxc','zxc','zxc','1683788297-img.png','Active','2023-05-11 00:00:00','2023-05-11 06:59:13'),(21,'eq','qe','qe','1683788328-img.png','Active','2023-05-11 00:00:00','2023-05-11 06:59:25'),(22,'fqwe','fqwe','fqwe','1683788418-img.jpg','Active','2023-05-11 00:00:00','2023-05-11 07:16:06'),(23,'fqwe','qưe','eqwe','1683788438-img.png','Active','2023-05-11 00:00:00','2023-05-11 07:16:06'),(24,'qưe','qưe','qưe','1683788456-img.png','Expired','2023-05-11 00:00:00',NULL),(25,'qr','qr','qr','1683789102-img.png','Active','2023-05-11 00:00:00',NULL),(26,'zxc','zxc','zxc','1683789292-img.png','Active','2023-05-11 07:14:52',NULL),(27,'Java','Java','Java','1683789392-img.png','Expired','2023-05-11 14:16:32',NULL);
+INSERT INTO `recruitments` VALUES (27,'1684341766-img.jpg','Active','2023-05-17 23:42:46',NULL),(28,'1684345055-img.jpg','Active','2023-05-17 23:43:25','2023-05-18 08:33:26'),(29,'1684373331-img.jpg','Active','2023-05-18 08:28:51',NULL),(30,'1684373457-img.png','Active','2023-05-18 08:30:57','2023-05-18 08:34:14'),(31,'1684373717-img.png','Active','2023-05-18 08:35:17',NULL),(32,'1684373763-img.jpg','Active','2023-05-18 08:36:03',NULL),(33,'1684373791-img.jpg','UnActive','2023-05-18 08:36:31','2023-05-18 08:38:39'),(34,'1684373814-img.webp','Active','2023-05-18 08:36:54','2023-05-18 08:38:40'),(35,'1684379524-img.png','Active','2023-05-18 10:12:04',NULL),(36,'1684380132-img.jpg','Active','2023-05-18 10:22:12',NULL),(37,'1684380213-img.jpg','Active','2023-05-18 10:23:33','2023-05-18 15:07:10'),(38,'1684380247-img.jpg','Active','2023-05-18 10:24:07','2023-05-18 15:05:50'),(39,'1684380304-img.jpg','Active','2023-05-18 10:25:04',NULL),(40,'1684391092-img.png','Active','2023-05-18 13:24:52',NULL),(41,'1684397128-img.jpg','Active','2023-05-18 15:05:28','2023-05-18 15:05:51'),(42,'1684403619-img.jpg','UnActive','2023-05-18 16:53:39',NULL);
 /*!40000 ALTER TABLE `recruitments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,7 +178,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'mienphi221@gmail.com','$2y$10$3/7pKwCky4xuYZs8dy3I5.KCXpF5KJ.clNC2n0cr/HR/0/cNWZWw.','49694e5a507a4c3142756e306945313350544c67');
+INSERT INTO `users` VALUES (1,'mienphi221@gmail.com','$2y$10$uiGAQ7MrlUy2dzbnMOBIFu3RbS/DNeegRxaYhdsoDbTwSXIY7/rci','52384b43653675584e30637a7236434865495253');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -167,4 +191,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-11 22:09:34
+-- Dump completed on 2023-05-18 19:07:01
