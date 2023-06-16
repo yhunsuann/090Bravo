fe14763a744c2fb323733025352286aa5b9266b3
ndhungvu_89 <ndhungvu@gmail.com>
2023-06-16 14:28:21 +0700

    update admin feature

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,7 +37,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/web.php'));
 
             Route::middleware('web')
-                ->prefix('admin1')
+                ->prefix('admin')
                 ->name('admin.')
                 ->group(base_path('routes/admin.php'));
         });
